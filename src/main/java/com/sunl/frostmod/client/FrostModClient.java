16e9beb8e471a54c8243d8cf0c81de97c9bb8855
sunlong <2013855675@qq.com>
2024-04-01 00:01:53 +0800

    first commit

---
+++ src/main/java/com/sunl/frostmod/client/FrostModClient.java
@@ -0,0 +1,6 @@
+package com.sunl.frostmod.client;/**
+* 描述
+* @author sunl
+* @date 2024/3/31
+*/public class FrostModClient {
+}
---
+++ src/main/java/com/sunl/frostmod/enchantment/FrostEnchantment.java
@@ -0,0 +1,6 @@
+package com.sunl.frostmod.enchantment;/**
+* 描述
+* @author sunl
+* @date 2024/3/31
+*/public class FrostEnchantment {
+}
